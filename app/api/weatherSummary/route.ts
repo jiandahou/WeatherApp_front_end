4c0a2d57908bc175c77b2169f42e61934951aad6
unknownfish1 <nodesireone@gmail.com>
2025-05-20 00:56:55 +0930

    AI Feature and animation

--- app/api/weatherSummary/route.ts
+++ app/api/weatherSummary/route.ts
@@ -5,6 +5,6 @@ import { GetWeatherSummary } from '@/app/action/serveractions'
 export async function POST(req: Request) {
   const body = await req.json();
   console.log("KEY? =>", !!process.env.OPENAI_API_KEY);
-  // const summary = await GetWeatherSummary(body);
-  return NextResponse.json({data:"2" });
+  const summary = await GetWeatherSummary(body);
+  return NextResponse.json({ summary });
 }
